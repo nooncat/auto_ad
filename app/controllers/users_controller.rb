cbb2297dc8c5cff448df3de5cef5e75c27ad6dc4
kom <kom01@rambler.ru>
2013-06-11 16:12:09 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
+#encoding: UTF-8
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Пользователь успешно создан!"
+      redirect_to root_url 
+    else
+      render 'new'
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,4 +11,29 @@ describe "UserPages" do
     it { should have_selector('h1', text: 'Новый пользователь') }
     it { should have_selector('title', text: full_title('Новый пользователь')) }
   end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Создать пользователя" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Логин",         with:  "Example User"
+        fill_in "Пароль",        with: "foobar"
+        fill_in "Подтверждение", with: "foobar"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
