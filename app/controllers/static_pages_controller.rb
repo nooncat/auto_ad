5f63482acf408db05b7d74cddcee687027d952ed
kom <kom01@rambler.ru>
2013-07-11 20:04:09 +0000

    mobile adaptive

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @cars = Car.paginate(page: params[:page])
+    @cars = Car.paginate(page: params[:page], per_page: 15)
     @cars_vip = Car.where("vip = ?", true).shuffle.first(3)
     @feedbacks = Feedback.where("checkf = ?", true).shuffle.first(2)
   end
