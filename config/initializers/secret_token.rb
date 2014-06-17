493f369f6025bf96b52d524dba0a45ab00a8c8d0
nooncat <kom01@rambler.ru>
2014-06-17 18:13:33 +0400

    add to gitignore

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AutoAd::Application.config.secret_token = 'd88f7282ec9c34568b2ff705e3add6aa1eebe895d8f3f2031794e919e3638d0ba8aa3eea6585fb626daf7d106283d0e63396a59e326811adb1bcd9bd163319f0'
