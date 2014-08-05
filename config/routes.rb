509911460b91df74ab344452b2b2234486ccc3fa
nooncat <kom01@rambler.ru>
2014-08-05 07:45:05 +0400

    pseudo pluralize ru errors

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ AutoAd::Application.routes.draw do
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
   match '/about',   to: 'static_pages#about'
-  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
