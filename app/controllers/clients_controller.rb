ba53d7b14b48c4d8c3884bb427ec7c860db9adae
nooncat <kom01@rambler.ru>
2014-09-30 23:35:51 +0400

    client page pre-end

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,15 +5,39 @@ class ClientsController < ApplicationController
   def create
     @client = Client.new(params[:client])
     if @client.save
-      flash[:success] = "Спасибо! Менеджер перезвонит Вам в ближайшее время."
+      if request.env["PATH_INFO"] == clients_path
+        flash[:success] = "Клиент сохранен."
+      else
+        flash[:success] = "Спасибо! Менеджер перезвонит Вам в ближайшее время."
+      end
     end
-    redirect_to root_url
+    redirect_to :back
   end
 
   def index
-    @callbacks = Client.paginate(page: params[:page], per_page: 10)
-    @clients = Client.paginate(page: params[:page], per_page: 10)
+    @callbacks = Client.where("information IS NULL or information = ''").order('created_at DESC').paginate(page: params[:callbacks_page], per_page: 12)
+    @clients = Client.where("information IS NOT NULL and information != ''").order('created_at DESC').paginate(page: params[:clients_page], per_page: 12)
     @client = Client.new
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update_attributes(params[:client])
+      flash[:success] = "Изменения сохранены."
+      redirect_to clients_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Client.find(params[:id]).destroy
+    flash[:success] = "Заявка удалена."
+    redirect_to :back
   end
 
   private
