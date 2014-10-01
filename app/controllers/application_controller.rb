0f32cee3ba1d8315a502738b6d30feede335242e
nooncat <kom01@rambler.ru>
2014-10-01 15:41:22 +0400

    finish client index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  
+  before_filter :check_callbacks
+
   def handle_unverified_request
     sign_out
     super
   end
+
+  def check_callbacks
+    if signed_in?
+      @callbacks_new = Client.where("information IS NULL or information = ''")
+    end
+  end
 end
+
